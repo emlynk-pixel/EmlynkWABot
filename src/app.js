14bc255c434273740f6157181de363811652b1f9
Shamal Sathsara <shamalsathsara4@gmail.com>
2026-09-21 18:42:21 +0530

    feat: add admin JWT login

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from "express";
+import authRoutes from "./routes/auth.js";
 
 
 const app = express();    //Create express application
 const PORT = 3000;
 
 app.use(express.json());
+
+app.use("/auth", authRoutes);
 
 app.get("/health", (req, res) => {
 
---
+++ src/routes/auth.js
@@ -0,0 +1,71 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+
+import prisma from "../config/prisma.js";
+import { comparePassword } from "../utils/password.js";
+
+const router = express.Router();
+
+// Admin login endpoint
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if email and pw are missing
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required",
+      });
+    }
+
+    //Find admin record from DB using email
+    const admin = await prisma.admin.findUnique({
+      where: { email },
+    });
+
+    //if admin is not found return generic login error
+    if (!admin || !admin.passwordHash) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    //compare the entered password with stored bcrypt hash
+    const passwordMatches = await comparePassword(
+      password,
+      admin.passwordHash
+    );
+
+    if (!passwordMatches) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    //Create JWT Token for successful login
+    const token = jwt.sign(
+      {
+        adminId: admin.adminId,
+        email: admin.email,
+        role: admin.role,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    return res.status(200).json({
+      message: "Login successful",
+      token,
+    });
+  } catch (error) {
+    console.error("Login error:", error);
+
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+});
+
+export default router;
